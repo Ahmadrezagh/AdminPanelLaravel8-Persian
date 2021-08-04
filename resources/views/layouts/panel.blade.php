4390acec3d655aa032f49cdb7efaeebd25e2d794
ahmadrezagh <ahmadreza.web@gmail.com>
2021-08-04 15:23:33 +0430

    admin panel menu button bug fixed

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -49,15 +49,9 @@
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
         <!-- Left navbar links -->
         <ul class="navbar-nav">
-{{--            <li class="nav-item">--}}
-{{--                <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item d-none d-sm-inline-block">--}}
-{{--                <a href="index3.html" class="nav-link">Home</a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item d-none d-sm-inline-block">--}}
-{{--                <a href="#" class="nav-link">Contact</a>--}}
-{{--            </li>--}}
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+            </li>
         </ul>
 
         <!-- SEARCH FORM -->
